2259e26749cf9237b08f14c29a602ccb8f18ce7a
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-02-12 03:19:59 +0530

    Day 4 - Modules

--- Day4/modules.js
+++ Day4/modules.js
@@ -1,8 +1,10 @@
 // const math = require('./math');
-const {add,sub} = require('./math');//Destructuring
+// const {add,sub} = require('./math');//Destructuring
+
+const math = require("fs")//Built in Modules
 
 // console.log(math);
 // console.log(math.add(2,3));
 // console.log(math.sub(5,3));
-console.log(add(2,3));
-console.log(sub(5,3));
+// console.log(add(2,3));
+// console.log(sub(5,3));
