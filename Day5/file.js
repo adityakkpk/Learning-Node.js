2092bad671ff4d81c8246e980c24096c99f3b487
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-02-13 03:06:45 +0530

    Reading Data from files

--- Day5/file.js
+++ Day5/file.js
@@ -1,10 +1,24 @@
 const fs = require('fs');
 
+//Write
 // Sync
 // fs.writeFileSync('./text.txt', "Hey There");//Creates a file and Overwrites the file
 
 // Async
 // fs.writeFile('./test.txt', "File write function", (err) => {});
 
-const result = fs.readFileSync("./contacts.txt", "utf-8");
-console.log(result);
+//Read
+//Sync
+// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// console.log(result);
+
+//Async
+// fs.readFile("./contacts.txt", "utf-8", (err, result)=> {
+//     if(err){
+//         console.log(err);
+//     }else {
+//         console.log(result);
+//     }
+// });
+
+//Appending
